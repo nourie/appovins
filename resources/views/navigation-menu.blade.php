9e5b85eaf9bf94a5c8a37cf05e6b089f37a9060b
nourie <nourie>
2022-11-27 14:26:31 +0100

    maj

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -13,7 +13,12 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Dashboard') }}  
+                    </x-jet-nav-link>
+                </div>
+                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('ovins.index') }}" :active="request()->routeIs('dashboard')">
+                        {{ __('site') }}  
                     </x-jet-nav-link>
                 </div>
             </div>
