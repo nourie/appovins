503cd89dae5001d608a73854ee91a17a61df49b1
nourie <nourie>
2022-12-06 10:35:33 +0100

    center menu dropdown

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -19,37 +19,37 @@
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
-    
-  
-
-
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
 
 
-               
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
 
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/ovins/public/ovins">الرئيسية</a>
-                    </li>
-                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ url('/dashboard') }}">لوحة التحكم</a>
-                    </li>
 
-                   
-                    <div class="dropdown">
-                        <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            شراء
-                        </button>
-                        <ul class="dropdown-menu">
+
+
+
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/ovins/public/ovins">الرئيسية</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{ url('/dashboard') }}">لوحة التحكم</a>
+                </li>
+
+
+                <div class="dropdown">
+                    <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        شراء
+                    </button>
+                    <ul class="dropdown-menu">
+                        <center>
                             @if (auth()->user()->userrole == 1)
                                 <li><a class="dropdown-item" href="{{ route('achat.achat') }}">شراء</a></li>
                                 <li><a class="dropdown-item" href="{{ route('achat.index') }}">قائمة الشراء</a></li>
@@ -61,53 +61,59 @@
                                 <li><a class="dropdown-item" href="{{ route('achat.indexavoir') }}">قائمة
                                         الإعادات</a></li>
                             @endif
+                        </center>
 
+                    </ul>
+                </div>
+                <div class="dropdown">
+                    <button class="btn btn-warning dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        بيع
+                    </button>
 
-                        </ul>
-                    </div>
-                    <div class="dropdown">
-                        <button class="btn btn-warning dropdown-toggle" type="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            بيع
-                        </button>
-                        <ul class="dropdown-menu">
+                    <ul class="dropdown-menu">
+                        <center>
                             @if (auth()->user()->userrole == 1)
                                 <li><a class="dropdown-item" href="{{ route('vente.index') }}">بيع</a></li>
                             @endif
                             <li><a class="dropdown-item" href="{{ route('vente.show', 1) }}">قائمة البيع</a></li>
-                        </ul>
-                    </div>
-                    <div class="dropdown">
-                        <button class="btn btn-success dropdown-toggle" type="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            قوائم
-                        </button>
-                        <ul class="dropdown-menu dropdown-menu-left">
-                            <li><a class="dropdown-item" href="{{ route('naissance.index') }}">الولادات</a></li>
+                        </center>
+                    </ul>
+
+                </div>
+                <div class="dropdown">
+                    <button class="btn btn-success dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        قوائم
+                    </button>
+                    <ul class="dropdown-menu ">
+                        <center>
+                            <li><a class="dropdown-item" href="{{ route('naissance.index') }}"> الولادات</a></li>
                             <li><a class="dropdown-item" href="{{ route('avorter.index') }}"> الاجهاضات</a></li>
                             <li><a class="dropdown-item" href="{{ route('die.index') }}"> الأغنام النافقة</a></li>
-                            <li><a class="dropdown-item" href="{{ route('die.index2') }}"> المواليد النافقة</a>
-                            </li>
-                        </ul>
-                    </div>
+                            <li><a class="dropdown-item" href="{{ route('die.index2') }}"> المواليد النافقة</a></li>
+                        </center>
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('ovins.index') }}">قائمة المرقمة</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('ovins.agneaulist') }}">المواليد</a>
-                    </li>
-                    <li class="nav-item">
-                    </li>
-                    <div>
-                        <form class="d-flex" action="{{ route('ovins.search') }}" method="GET" role="search">
-                            {{ method_field('get') }}
-                            {{ csrf_field() }}
-                            <input class="form-control me-2" type="search" placeholder="بحث" aria-label="البجث"
-                                name="q">
-                            <button class="btn btn-outline-success" type="submit">إبحث</button>
-                        </form>
-                    </div>
+                    </ul>
+                </div>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('ovins.index') }}">قائمة المرقمة</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('ovins.agneaulist') }}">المواليد</a>
+                </li>
+                <li class="nav-item">
+                </li>
+                <div>
+                    <form class="d-flex" action="{{ route('ovins.search') }}" method="GET" role="search">
+                        {{ method_field('get') }}
+                        {{ csrf_field() }}
+                        <input class="form-control me-2" type="search" placeholder="بحث" aria-label="البجث"
+                            name="q">
+                        <button class="btn btn-outline-success" type="submit">إبحث</button>
+                    </form>
+                </div>
             </ul>
         </div>
     </nav>
--- storage/framework/views/c8a8efdcfb6357a9783082b014e769c7f4a73fd7.php
+++ storage/framework/views/c8a8efdcfb6357a9783082b014e769c7f4a73fd7.php
@@ -4,7 +4,7 @@
 <head>
     <title> تتبع القطيع - <?php echo $__env->yieldContent('title'); ?></title>
     <meta charset="utf-8">
-    <meta name="viewport " content="width=device-width, initial-scale=1">
+    <meta name="viewport " content="width=device-width, initial-scale=.8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js"
@@ -17,107 +17,107 @@
 
 <body>
 
-    <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
 
 
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-
-                        <?php if(Route::has('login')): ?>
-                            <div class="nav-link active" aria-current="page">
-                                <?php if(auth()->guard()->check()): ?>
-                                    <a href="<?php echo e(url('/dashboard')); ?>"
-                                        class="text-sm text-gray-700 dark:text-gray-500 underline">لوحة التحكم
-                                        <div class="row">
-                                        </div>
-                                    <?php else: ?>
-                                        <a href="<?php echo e(route('login')); ?>"
-                                            class="text-sm text-gray-700 dark:text-gray-500 underline">تسجل الدخول</a>
-
-                                        <?php if(Route::has('register')): ?>
-                                            <a href="<?php echo e(route('register')); ?>"
-                                                class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">التسجيل </a>
-                                        <?php endif; ?>
-                                    <?php endif; ?>
-                        <?php endif; ?>
-                    </li>
-
-             
-
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="/ovins/public/ovins">الرئيسية</a>
-                        </li>
 
 
-                        <div class="dropdown">
-                            <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown"
-                                aria-expanded="false">
-                                شراء
-                            </button>
-                            <ul class="dropdown-menu">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+
+
+
+
+
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/ovins/public/ovins">الرئيسية</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="<?php echo e(url('/dashboard')); ?>">لوحة التحكم</a>
+                </li>
+
+
+                <div class="dropdown">
+                    <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        شراء
+                    </button>
+                    <ul class="dropdown-menu">
+                        <center>
+                            <?php if(auth()->user()->userrole == 1): ?>
                                 <li><a class="dropdown-item" href="<?php echo e(route('achat.achat')); ?>">شراء</a></li>
                                 <li><a class="dropdown-item" href="<?php echo e(route('achat.index')); ?>">قائمة الشراء</a></li>
                                 <li><a class="dropdown-item" href="<?php echo e(route('achat.avoir')); ?>">إعادة</a></li>
-                                <li><a class="dropdown-item" href="<?php echo e(route('achat.indexavoir')); ?>">قائمة الإعادات</a>
-                                </li>
+                                <li><a class="dropdown-item" href="<?php echo e(route('achat.indexavoir')); ?>">قائمة
+                                        الإعادات</a></li>
+                            <?php else: ?>
+                                <li><a class="dropdown-item" href="<?php echo e(route('achat.index')); ?>">قائمة الشراء</a></li>
+                                <li><a class="dropdown-item" href="<?php echo e(route('achat.indexavoir')); ?>">قائمة
+                                        الإعادات</a></li>
+                            <?php endif; ?>
+                        </center>
 
+                    </ul>
+                </div>
+                <div class="dropdown">
+                    <button class="btn btn-warning dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        بيع
+                    </button>
 
-                            </ul>
-                        </div>
-                        <div class="dropdown">
-                            <button class="btn btn-warning dropdown-toggle" type="button" data-bs-toggle="dropdown"
-                                aria-expanded="false">
-                                بيع
-                            </button>
-                            <ul class="dropdown-menu">
+                    <ul class="dropdown-menu">
+                        <center>
+                            <?php if(auth()->user()->userrole == 1): ?>
                                 <li><a class="dropdown-item" href="<?php echo e(route('vente.index')); ?>">بيع</a></li>
-                                <li><a class="dropdown-item" href="<?php echo e(route('vente.show', 1)); ?>">قائمة البيع</a></li>
-                            </ul>
-                        </div>
-                        <div class="dropdown">
-                            <button class="btn btn-success dropdown-toggle" type="button" data-bs-toggle="dropdown"
-                                aria-expanded="false">
-                                قوائم
-                            </button>
-                            <ul class="dropdown-menu dropdown-menu-left">
-                                <li><a class="dropdown-item" href="<?php echo e(route('naissance.index')); ?>">الولادات</a></li>
-                                <li><a class="dropdown-item" href="<?php echo e(route('avorter.index')); ?>"> الاجهاضات</a></li>
-                                <li><a class="dropdown-item" href="<?php echo e(route('die.index')); ?>"> الأغنام النافقة</a></li>
-                                <li><a class="dropdown-item" href="<?php echo e(route('die.index2')); ?>"> المواليد النافقة</a>
-                                </li>
-                            </ul>
-                        </div>
+                            <?php endif; ?>
+                            <li><a class="dropdown-item" href="<?php echo e(route('vente.show', 1)); ?>">قائمة البيع</a></li>
+                        </center>
+                    </ul>
 
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?php echo e(route('ovins.index')); ?>">قائمة المرقمة</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?php echo e(route('ovins.agneaulist')); ?>">المواليد</a>
-                        </li>
-                        <li class="nav-item">
-                        </li>
-                        <div>
-                            <form class="d-flex" action="<?php echo e(route('ovins.search')); ?>" method="GET" role="search">
-                                <?php echo e(method_field('get')); ?>
+                </div>
+                <div class="dropdown">
+                    <button class="btn btn-success dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        قوائم
+                    </button>
+                    <ul class="dropdown-menu ">
+                        <center>
+                            <li><a class="dropdown-item" href="<?php echo e(route('naissance.index')); ?>"> الولادات</a></li>
+                            <li><a class="dropdown-item" href="<?php echo e(route('avorter.index')); ?>"> الاجهاضات</a></li>
+                            <li><a class="dropdown-item" href="<?php echo e(route('die.index')); ?>"> الأغنام النافقة</a></li>
+                            <li><a class="dropdown-item" href="<?php echo e(route('die.index2')); ?>"> المواليد النافقة</a></li>
+                        </center>
 
-                                <?php echo e(csrf_field()); ?>
+                    </ul>
+                </div>
 
-                                <input class="form-control me-2" type="search" placeholder="بحث" aria-label="البجث"
-                                    name="q">
-                                <button class="btn btn-outline-success" type="submit">إبحث</button>
-                            </form>
-                        </div>
-            </div>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo e(route('ovins.index')); ?>">قائمة المرقمة</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo e(route('ovins.agneaulist')); ?>">المواليد</a>
+                </li>
+                <li class="nav-item">
+                </li>
+                <div>
+                    <form class="d-flex" action="<?php echo e(route('ovins.search')); ?>" method="GET" role="search">
+                        <?php echo e(method_field('get')); ?>
+
+                        <?php echo e(csrf_field()); ?>
+
+                        <input class="form-control me-2" type="search" placeholder="بحث" aria-label="البجث"
+                            name="q">
+                        <button class="btn btn-outline-success" type="submit">إبحث</button>
+                    </form>
+                </div>
+            </ul>
+        </div>
     </nav>
 </body>
 
